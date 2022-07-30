21ca250ed1349bc5ba42b5b2e64a88f025b46723
lsoulworkerl <prosto2002vitalik@gmail.com>
2022-07-30 05:59:34 +0300

    Edit Data class - add function to edit data in db

--- database.py
+++ database.py
@@ -71,6 +71,78 @@ class Data:
             )
             connection.autocommit = True
             
+            #data before changes
+            start_list = self.get_pickle_data()
+            user_data = start_list[0]
+            
+            #save id user
+            user_id = start_list[0][0]
+            
+            #edit list, delete user login data
+            start_list.pop(0)
+            note_list.pop(0)
+            
+            #get id for delete list
+            temp_old = []
+            temp_new = []
+            for i in start_list:
+                temp_old.append(i[0])
+            
+            for i in note_list:
+                temp_new.append(i[0])
+            
+            id_list_delete = list(set(temp_new) ^ set(temp_old))
+            
+            #delete elements
+            for i in id_list_delete:
+                with connection.cursor() as cursor:
+                    cursor.execute(
+                        """DELETE FROM notation WHERE id = '{0}'""".format(i)
+                    )
+            
+            #get id for change
+            id_list_change = list(set(temp_new) & set(temp_old))
+            
+            for i in id_list_delete:
+                for l in start_list:
+                    if i == l[0]:
+                        start_list.remove(l)
+                        break
+                    
+            id_list_change = []
+            
+            for i in range(0, len(start_list)):
+                if start_list[i] != note_list[i]:
+                    id_list_change.append(start_list[i][0]) 
+            
+            #change data
+            for i in id_list_change:
+                for l in note_list:
+                    if i == l[0]:
+                        name = l[1]
+                        text = l[2]
+                        with connection.cursor() as cursor:
+                            cursor.execute(
+                                """UPDATE notation SET name = {0}, 
+                                text = {1} WHERE id = '{2}'""".format(name, text, i)
+                            )
+                    break
+            
+            #add new data
+            for i in note_list:
+                if i[0] == -1:
+                    with connection.cursor() as cursor:
+                        cursor.execute(
+                            """INSERT INTO notation (name, text, id_users) VALUES 
+                            (%s, %s, %s)""", (i[1], i[2], user_id)
+                        )   
+            
+            #add user data to list
+            note_list.insert(0, user_data)
+            
+            #edit pickle data
+            self.change_pickle_data(note_list)
+            
         except Exception as _ex:
             print("[INFO] Error while working with PostgreSQL", _ex)
         finally:
